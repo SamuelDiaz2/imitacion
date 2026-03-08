47e788d85cc543f8bb3fbd59a64bb961e9d9030d
Juan1624 <juanestebanarias164@gmail.com>
2026-03-08 16:55:15 +0000

    Implementacion de configuracion porsche

--- porsche/src/App.jsx
+++ porsche/src/App.jsx
@@ -1,4 +1,5 @@
 import Footer from "./Footer"
+import PorscheMiniconfig from "./componentes/PorscheMiniconfig";
 import PorscheSection from "./componentes/PorscheSection";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 return (
   <>
   <PorscheSection />
+  <PorscheMiniconfig/>
   <Footer/>
   </>
 )
