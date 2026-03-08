84c087d99d87d8f89fbe9b55fb1b8368ad85aca7
SamuelDiaz2 <samuel.diazf@uniagustiniana.edu.co>
2026-03-08 22:59:10 +0000

    componente header

--- porsche/src/App.jsx
+++ porsche/src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from "./Footer"
 import PorscheMiniconfig from "./componentes/PorscheMiniconfig";
 import PorscheSection from "./componentes/PorscheSection";
-import PorscheDiscovery from "./Components/PorscheDiscovery"
+import PorscheDiscovery from "./Components/PorscheDiscovery";
+import Porscheheader from "./componentes/PorscheHeader";
 
 function App() {
 
 return (
   <>
+  <Porscheheader />
   <PorscheSection />
   <PorscheMiniconfig/>
   <PorscheDiscovery />
